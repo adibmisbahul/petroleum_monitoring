a688be08390bf0edb95f0aeaec9a02232400cc50
adibmisbahul <adibmisbahul101812@gmail.com>
2025-12-22 14:25:09 -0300

    create dashboard layouts

---
+++ src/layouts/dashboard.jsx
@@ -0,0 +1,11 @@
+import "./dashboard.css";
+
+export default function Dashboard() {
+  return (
+    <div className="container">
+      <div className="sidebar">sidebar</div>
+      <div className="header">header</div>
+      <div className="main">main</div>
+    </div>
+  );
+}
